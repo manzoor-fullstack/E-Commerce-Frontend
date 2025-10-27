f9ae3b499e43641b21888907ab309a5c3994c13f
Manzoor Ahmed <manzoorjam467@gmail.com>
2025-10-27 15:12:36 +0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    
+    <h1>Hello world!</h1>
+   <Button>Click me</Button>
+   </div>
+  )
+}
